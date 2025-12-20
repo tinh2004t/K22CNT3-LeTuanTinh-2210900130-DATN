a3a150ca1bfee7e19c909dc7531fd0b803310580
tinh2004t <117645071+tinh2004t@users.noreply.github.com>
2025-12-20 16:20:27 +0700

    20/12/25
    Save game - player data

--- Assets/SCRIPTS/MainMenu.cs
+++ Assets/SCRIPTS/MainMenu.cs
@@ -1,8 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Button LoadGameBTN;
+
+    private void Start()
+    {
+        LoadGameBTN.onClick.AddListener(() =>
+        {
+            SaveManager.Instance.StartLoadedGame();
+        });
+    }
+
     public void NewGame()
     {
         SceneManager.LoadScene("GameScene");
