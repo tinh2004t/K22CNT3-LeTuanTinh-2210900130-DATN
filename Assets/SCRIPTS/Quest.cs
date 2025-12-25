bfa97fb46850b9a0bcd8156a2d02f20cdaa93757
tinh2004t <117645071+tinh2004t@users.noreply.github.com>
2025-12-25 14:05:07 +0700

    25/12/25
    npc quest

---
+++ Assets/SCRIPTS/Quest.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Quest
+{
+    [Header("Bools")]
+    public bool accepted;
+    public bool declined;
+    public bool initialDialogCompleted;
+    public bool isCompleted;
+
+    public bool hasNoRequirements;
+
+    [Header("Quest Info")]
+    public QuestInfo info;
+
+
+}
