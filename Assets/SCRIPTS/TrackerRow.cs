ada8723fe24953897ce4343b1b62d841aa3aa67f
tinh2004t <117645071+tinh2004t@users.noreply.github.com>
2025-12-29 06:47:28 +0700

    28/12/25

---
+++ Assets/SCRIPTS/TrackerRow.cs
@@ -0,0 +1,9 @@
+using TMPro;
+using UnityEngine;
+
+public class TrackerRow : MonoBehaviour
+{
+    public TextMeshProUGUI questName;
+    public TextMeshProUGUI description;
+    public TextMeshProUGUI requirements;
+}
