9eeebbd32387455e2d194178fc8c89af7a4658ff
LeTuanTinh <117645071+tinh2004t@users.noreply.github.com>
2026-04-04 08:41:29 +0700

    Update main menu
    - Load menu
    - Setting menu
    - Version text

---
+++ Assets/SCRIPTS/GameVersionDisplay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class GameVersionDisplay : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI versionText;
+
+    private void Start()
+    {
+        if (versionText != null)
+        {
+            versionText.text = "Version " + Application.version;
+        }
+        else
+        {
+            Debug.LogWarning("Chưa gán VersionText vào script!");
+        }
+    }
+}
