af7b7314e4fae6a8ada06e533c2070cf426a45a0
LeTuanTinh <117645071+tinh2004t@users.noreply.github.com>
2026-04-07 15:39:38 +0700

    Update new map
    - new terrain
    - add teleport gate
    - new dana's house

---
+++ Assets/SCRIPTS/TeleportDoor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class TeleportDoor : MonoBehaviour
+{
+    [Header("Điểm đến")]
+    [Tooltip("Kéo thả GameObject đích đến vào đây")]
+    public Transform destination;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            TeleportPlayer(other.gameObject);
+        }
+    }
+
+    private void TeleportPlayer(GameObject player)
+    {
+        CharacterController controller = player.GetComponent<CharacterController>();
+
+        if (controller != null)
+        {
+            controller.enabled = false;
+            player.transform.position = destination.position;
+            controller.enabled = true;
+        }
+        else
+        {
+            player.transform.position = destination.position;
+        }
+
+        Debug.Log("Đã dịch chuyển người chơi!");
+    }
+}
