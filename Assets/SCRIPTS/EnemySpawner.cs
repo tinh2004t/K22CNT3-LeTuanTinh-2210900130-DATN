0d98767b43a14c60d70bb5ad14453055b0347252
LeTuanTinh <117645071+tinh2004t@users.noreply.github.com>
2026-04-10 19:20:52 +0700

    Update command

--- Assets/SCRIPTS/EnemySpawner.cs
+++ Assets/SCRIPTS/EnemySpawner.cs
@@ -1,38 +1,81 @@
 using UnityEngine;
+using System.Collections;
 
 public class EnemySpawner : MonoBehaviour
 {
+    public enum SpawnTimeOption { Anytime, DayOnly, NightOnly }
+
     [SerializeField] private GameObject enemyPrefab;
     [SerializeField] private float respawnDelay = 10f;
+    [SerializeField] private SpawnTimeOption spawnTimeRequirement = SpawnTimeOption.Anytime;
 
     private GameObject currentEnemy;
 
     private void Start()
     {
-        SpawnEnemy();
+        StartCoroutine(SpawnProcess());
+    }
+
+    private IEnumerator SpawnProcess()
+    {
+        while (true)
+        {
+            if (currentEnemy == null && CanSpawnNow())
+            {
+                SpawnEnemy();
+            }
+
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+    private bool CanSpawnNow()
+    {
+        if (DayNightSystem.Instance == null) return true;
+
+        bool isNight = DayNightSystem.Instance.IsNight();
+
+        switch (spawnTimeRequirement)
+        {
+            case SpawnTimeOption.DayOnly:
+                return !isNight;
+            case SpawnTimeOption.NightOnly:
+                return isNight;
+            default:
+                return true;
+        }
     }
 
     private void SpawnEnemy()
     {
-        if (enemyPrefab == null)
-        {
-            Debug.LogWarning("Enemy Prefab is not assigned in the EnemySpawner");
-            return;
-        }
+        if (enemyPrefab == null) return;
 
         currentEnemy = Instantiate(enemyPrefab, transform.position, transform.rotation);
-        currentEnemy.GetComponent<Animal>().OnDestroyed += HandleEnemyDeath;
-    }
 
-    private void OnDestroy()
-    {
-        currentEnemy.GetComponent<Animal>().OnDestroyed -= HandleEnemyDeath;
-
+        Animal animalScript = currentEnemy.GetComponent<Animal>();
+        if (animalScript != null)
+        {
+            animalScript.OnDestroyed += HandleEnemyDeath;
+        }
     }
 
     private void HandleEnemyDeath()
     {
+        if (currentEnemy != null)
+        {
+            Animal animalScript = currentEnemy.GetComponent<Animal>();
+            if (animalScript != null) animalScript.OnDestroyed -= HandleEnemyDeath;
+        }
+
         currentEnemy = null;
-        Invoke(nameof(SpawnEnemy), respawnDelay);
+
+        StopAllCoroutines();
+        StartCoroutine(DelayedRestart());
+    }
+
+    private IEnumerator DelayedRestart()
+    {
+        yield return new WaitForSeconds(respawnDelay);
+        StartCoroutine(SpawnProcess());
     }
 }
