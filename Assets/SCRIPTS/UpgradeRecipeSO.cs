4e2bf4a9a905e7fe262c7438a645efd16dd4f653
LeTuanTinh <117645071+tinh2004t@users.noreply.github.com>
2026-04-19 16:07:36 +0700

    upd

---
+++ Assets/SCRIPTS/UpgradeRecipeSO.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public struct MaterialRequirement
+{
+    public ItemSO item; // Cấu trúc ItemSO hiện tại của game bạn
+    public int amount;
+}
+
+[CreateAssetMenu(fileName = "New Upgrade Recipe", menuName = "Survival Game/Upgrade Recipe")]
+public class UpgradeRecipeSO : ScriptableObject
+{
+    public string recipeName;
+    public ItemSO baseItem;             // Vật phẩm gốc (VD: Kiếm sắt)
+    public ItemSO upgradedItem;         // Vật phẩm sau khi nâng cấp (VD: Kiếm thép)
+
+    public int coinCost;                // Số vàng yêu cầu
+    public List<MaterialRequirement> requiredMaterials; // Danh sách nguyên liệu
+
+    public GameObject upgradeEffect;    // Hiệu ứng hạt (Particle) khi nâng cấp thành công
+}
